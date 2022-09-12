665c27f545ae5d35f7e9c80335f3ed0d4cc3bf2c
amine <aminemalyana12@gmail.com>
2022-09-12 14:55:02 +0100

    chalenge 1

---
+++ periode-sas-semaine-2/les-boucles/challenge1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int N , i;
+    printf("entree un nomber de 1 a 10 :");
+    scanf("%d",&N);
+
+
+    for (i=1 ; i<=10 ; i++ )
+
+
+        printf("%d x %d = %d \n",N,i,N*i);
+
+
+
+
+
+
+
+
+
+    return 0;
+}
